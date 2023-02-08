e9323890ea088f678f903526119d43102872b977
Swarada Bhosale <swaradapb113@gmail.com>
2023-02-08 17:10:23 +0530

    Made the necessary changes to remove the errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,6 @@
 import './App.css'
 import Login from './Login'
-import Procedure from './Procedure'
 function App() {
-<<<<<<< HEAD
-  return (<Login />), (<Procedure />)
-=======
-  return (
-  <Login/>
-  )
->>>>>>> refs/remotes/origin/main
+  return <Login />
 }
-
 export default App
