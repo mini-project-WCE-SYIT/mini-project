feeca5ee3dbb317227ade703da85dad69876fa05
Swarada Bhosale <swaradapb113@gmail.com>
2023-02-08 17:00:29 +0530

    Made the frontend of procedure page responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,7 @@ import './App.css'
 import Login from './Login'
 import Procedure from './Procedure'
 function App() {
-  return (
-  <Login />
-  )
+  return (<Login />), (<Procedure />)
 }
 
 export default App
